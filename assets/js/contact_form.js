a9e79c35ffcbd56c958da57b494bc442d38b1a30
midhunhk <2026817+midhunhk@users.noreply.github.com>
2022-05-19 13:17:55 -0400

    update layout

--- assets/js/contact_form.js
+++ assets/js/contact_form.js
@@ -33,3 +33,13 @@
     contactForm.addEventListener("submit", sendForm)
 
 })()
+
+const scrollToSection = (section) => {
+    const element = document.getElementById(section)
+    const y = element.getBoundingClientRect().top + window.scrollY - 60;
+    window.scroll({
+    top: y,
+    behavior: 'smooth'
+    });
+    return false
+}
